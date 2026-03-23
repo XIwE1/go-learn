e41e81914b0f09e641fd934e44a4a9bb0447c59c
XIwE1 <804393870@qq.com>
2026-03-24 04:26:57 +0800

    feat:增加GET路径参数查询接口

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -34,16 +35,58 @@ func CORSHandler() gin.HandlerFunc {
 // 	}
 // }
 
+type User struct {
+	// uri 结构体标签将 URI 路径参数直接绑定到结构体中
+	Name string `uri:"name" binding:"required"`
+	Id   int    `uri:"id" binding:"required"`
+
+	// `xx:"yy"` = 结构体标签（Struct Tag）。它是一种元数据（关于数据的数据），用来为结构体的字段提供额外的信息
+	// Name string `json:"name"`
+	// 当把 User 结构体转换成 JSON 字符串（序列化）时，字段 Name 在 JSON 中应该使用键名 "name"，而不是默认的字段名 "Name"。
+	// 类似的还有
+	// gorm:"column:user_name;type:varchar(100)" 用来指定数据库表中的列名和类型
+}
+
 func main() {
 	router := gin.Default()
 	// 使用中间件
 
 	router.Use(Logger())
 	router.Use(CORSHandler())
+	// 测试接口
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	// 查询用户接口
+	router.GET("/user/info/:name/:id", func(c *gin.Context) {
+		var user User
+		// √ 使用 Gin 的绑定机制 将请求参数直接绑定到结构体，并**自动进行类型转换和校验**
+		if err := c.ShouldBindUri(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "路径参数不合法",
+				"error":   err.Error(),
+			})
+			return
+		}
+		// 在处理函数内使用 c.Param("name") 来获取路径参数的值
+		userName := user.Name
+		userId := user.Id
+		if userName == "" || userId <= 0 {
+			c.JSON(http.StatusBadRequest, "无效的数据")
+			return
+		}
+
+		// × 使用 strconv.Atoi 最基础的方法，但需要在每个 handler 中重复编写错误处理代码
+		// id, err := strconv.Atoi(idStr)
+		// if err != nil {
+		// 	c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		// 	return
+		// }
+
+		c.JSON(http.StatusOK, user)
 	})
 	router.Run() // listens on 0.0.0.0:8080 by default
 }
