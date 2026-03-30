f348141248f425975c782b4cfc4833273ef602c1
XIwE1 <804393870@qq.com>
2026-03-31 05:18:52 +0800

    feat:测试shouldbindbodywith将请求体绑定到不同的结构体

--- routes/orders.go
+++ routes/orders.go
@@ -4,7 +4,31 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
+
+type GMOrder struct {
+	Target string `json:"target"`
+}
+
+type PlayerOrder struct {
+	Id string `json:"id"`
+}
+
+func executeOrder(c *gin.Context) {
+	var gmOrders GMOrder
+	var PlayerOrders PlayerOrder
+
+	// × ShouldBind  会消费 c.Request.Body，导致后续ShouldBindxx失效
+	// if errGM := c.ShouldBind(&gmOrders); errGM != nil {
+	// √ 使用 ShouldBindBodyWith 它会读取一次请求体并将其存储在上下文中，后续的绑定重用请求体
+	if errGM := c.ShouldBindBodyWith(&gmOrders, binding.JSON); errGM != nil {
+		// ...
+	}
+	if errPlayer := c.ShouldBindBodyWith(&PlayerOrders, binding.JSON); errPlayer != nil {
+		// ...
+	}
+}
 
 func listOrders(c *gin.Context)  { c.JSON(http.StatusOK, gin.H{"action": "list_orders"}) }
 func createOrder(c *gin.Context) { c.JSON(http.StatusCreated, gin.H{"action": "create_order"}) }
